1c047694afdd9acec49732fd491d45dc40a5e5c4
Gal Dubitski <gdubitski@sapient.com>
2019-01-04 15:51:43 +0000

    screen changes

--- src/containers/MyProfileContainer/contact-details.js
+++ src/containers/MyProfileContainer/contact-details.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import { Thumbnail, Text, Body, Tab, Tabs } from "native-base";
+import {
+  Left, Right,
+  Container, Header, Content, List, ListItem, View, Text, SwipeRow, Button, Icon } from "native-base";
 // import styles from "./styles";
 
 export default class ContactDetails extends Component {
   render() {
+    const listItemStyle = { flex: 1, justifyContent: "space-between" };
     return (
-      <Text>Here goes the contact details</Text>
+      <View>
+        <List>
+          <ListItem style={listItemStyle}>
+            <Text>Email</Text>
+            <Text style={{ fontWeight: "bold" }}>liam.pitchford@ittf.com</Text>
+          </ListItem>
+          <ListItem style={listItemStyle}>
+            <Text>Mobile</Text>
+            <Text style={{ fontWeight: "bold" }}>079-76787215</Text>
+          </ListItem>
+          <ListItem style={listItemStyle}>
+            <Text>Date of Birth</Text>
+            <Text style={{ fontWeight: "bold" }}>04/09/1985</Text>
+          </ListItem>
+          <ListItem style={listItemStyle}>
+            <Text>Postcode</Text>
+            <Text style={{ fontWeight: "bold" }}>NW9 4ET</Text>
+          </ListItem>
+        </List>
+      </View>
     );
   }
 }
